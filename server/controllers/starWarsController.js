507f3e93cc3602b84d136587f9d89733ac169cf3
Kevin Li <128013923+myttins@users.noreply.github.com>
2023-07-08 16:48:52 -0700

    mongo branch challenges

--- server/controllers/starWarsController.js
+++ server/controllers/starWarsController.js
@@ -1,35 +1,87 @@
-const models = require('../models/starWarsModels');
+const db = require('../models/starWarsModels');
 
 const starWarsController = {};
 
-starWarsController.getCharacters = (req, res, next) => {
-  // write code here
+starWarsController.getCharacters = async (req, res, next) => {
+  try {
+    // Get results of all documents in Person model
+    // Save results into res.locals and move onto next middleware
+    const data = await db.Person.find({});
 
-  next();
+    res.locals.characters = data;
+    return next();
+  } catch {
+    return next({
+      log: 'starWarsController.getCharacters ERROR: Person query failure',
+      message: { err: 'starWarsController.getCharacters error' },
+    });
+  }
 };
 
-starWarsController.getSpecies = (req, res, next) => {
-  // write code here
-
-  next();
+starWarsController.getSpecies = async (req, res, next) => {
+  try {
+    const data = await db.Species.find({ _id: req.query.id });
+    res.locals.species = data[0];
+    return next();
+  } catch {
+    return next({
+      log: 'starWarsController.getSpecies',
+      message: { err: 'starWarsController.getSpecies' },
+    });
+  }
 };
 
-starWarsController.getHomeworld = (req, res, next) => {
-  // write code here
-
-  next();
+starWarsController.getHomeworld = async (req, res, next) => {
+  try {
+    const data = await db.Planet.find({ _id: req.query.id });
+    res.locals.planet = data[0];
+    return next();
+  } catch {
+    return next({
+      log: 'starWarsController.getHomeworld',
+      message: { err: 'starWarsController.getHomeworld' },
+    });
+  }
 };
 
-starWarsController.getFilm = (req, res, next) => {
-  // write code here
-
-  next();
+starWarsController.getFilm = async (req, res, next) => {
+  try {
+    const data = await db.Film.find({ _id: req.query.id });
+    res.locals.film = data[0];
+    return next();
+  } catch {
+    return next({
+      log: 'starWarsController.getFilm',
+      message: { err: 'starWarsController.getFilm' },
+    });
+  }
 };
 
-starWarsController.addCharacter = (req, res, next) => {
-  // write code here
-
-  next();
+starWarsController.addCharacter = async (req, res, next) => {
+  try {
+    const body = {
+      name: req.body.name,
+      gender: req.body.gender,
+      species: req.body.species,
+      species_id: req.body.species_id,
+      birth_year: req.body.birth_year,
+      eye_color: req.body.eye_color,
+      skin_color: req.body.skin_color,
+      hair_color: req.body.hair_color,
+      mass: req.body.mass,
+      height: req.body.height,
+      homeworld: req.body.homeworld,
+      homeworld_id: req.body.homeworld_id,
+      films: req.body.films,
+    };
+    await db.Person.insertMany(body);
+    return next();
+  } catch {
+    return next({
+      log: 'starWarsController.addCharacter',
+      message: { err: 'starWarsController.addCharacter' },
+    });
+  }
 };
 
 module.exports = starWarsController;
--- server/routes/api.js
+++ server/routes/api.js
@@ -6,22 +6,22 @@ const router = express.Router();
 
 router.get('/',
   starWarsController.getCharacters,
-  (req, res) => res.status(200).json([])
+  (req, res) => res.status(200).json(res.locals.characters)
 );
 
 router.get('/species',
   starWarsController.getSpecies,
-  (req, res) => res.status(200).json({})
+  (req, res) => res.status(200).json(res.locals.species)
 );
 
 router.get('/homeworld',
   starWarsController.getHomeworld,
-  (req, res) => res.status(200).json({})
+  (req, res) => res.status(200).json(res.locals.planet)
 );
 
 router.get('/film',
   starWarsController.getFilm,
-  (req, res) => res.status(200).json({})
+  (req, res) => res.status(200).json(res.locals.film)
 );
 
 router.post('/character',
